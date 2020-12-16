abc6e5559447f8bb100cef1d12151dff872886cc
thaliadettenborn <thalia.born@gmail.com>
2020-12-16 19:33:51 -0300

    adding nav at pages create and view test

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import Header from './Header';
 import Button from './Button';
+import Nav from './Nav';
 
 
 export {
   Header,
   Button,
+  Nav
 }
--- src/pages/CreateTest/index.js
+++ src/pages/CreateTest/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Nav } from '../../components';
 
 export default function CreateTest() {
   return (
-    <>
+    <Container>
+      <Nav />
       Em breve
-    </>
+    </Container>
   )
 }
+
+const Container = styled.main`
+  margin-top: 120px;
+`;
